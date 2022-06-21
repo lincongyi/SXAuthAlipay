8d3aa86d0fa6cac30af9cc485628a88c6f46f403
congyi <88583354@163.com>
2022-06-22 00:33:47 +0800

    fixed merge

--- src/api/demo/index.js
+++ src/api/demo/index.js
@@ -4,7 +4,7 @@ import request from '@/utils/request'
  */
 export function getAccessToken(params) {
   return request({
-    baseURL: process.env.NODE_ENV === 'production' ? import.meta.env.VITE_AUTH_BASE_URL : import.meta.env.VITE_DEMO_BASE_URL,
+    baseURL: import.meta.env.VITE_DEMO_BASE_URL,
     url: '/getaccesstoken',
     method: 'get',
     params
@@ -16,7 +16,7 @@ export function getAccessToken(params) {
  */
 export function getCertToken(data) {
   return request({
-    baseURL: process.env.NODE_ENV === 'production' ? import.meta.env.VITE_AUTH_BASE_URL : import.meta.env.VITE_DEMO_BASE_URL,
+    baseURL: import.meta.env.VITE_DEMO_BASE_URL,
     url: '/authreq',
     method: 'post',
     data
