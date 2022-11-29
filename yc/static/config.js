742bf1716db5217a9d496e5a3c7cf9008cc37ab9
lincongyi <88583354@163.com>
2022-11-29 15:05:41 +0800

    update

--- yc/static/config.js
+++ yc/static/config.js
@@ -8,7 +8,7 @@ window.DEV = {
     app_secret: '12345'
   },
   successUrl: encodeURIComponent('/'), // 成功的回调地址, 可通过url传参方式传入, url传参方式优先级高于配置文件
-  successDelayTime: 3000, // 检测成功之后延迟多久跳转
+  successDelayTime: 2000, // 检测成功之后延迟多久跳转
   failedUrl: encodeURIComponent('/'), // 失败的回调地址, 可通过url传参方式传入, url传参方式优先级高于配置文件
   returnUrl: encodeURIComponent('/'), // 返回的URL, 可通过url传参方式传入, url传参方式优先级高于配置文件
   recordTime: 5000, // 默认5000毫秒, 建议大于1000 小于10000
